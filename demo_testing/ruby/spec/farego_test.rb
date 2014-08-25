37462599eeef14b2c8529d0f7c1d5efb3432411b
dir <dir@idiri.net>
2014-08-25 11:42:58 +0800

    Add Test "farego"
    Add Test "farego"

--- demo_testing/ruby/spec/farego_test.rb
+++ demo_testing/ruby/spec/farego_test.rb
@@ -21,22 +21,49 @@ describe 'the page loads' do
     arrCity.text.must_equal '不限定'
     searchBtn = b.input(id: 'btnSearch')
     searchBtn.value.must_equal '搜 尋'
-
-
     selday = b.span(id: 'selStay')
     selday.text.must_equal '2'
-    # selday.click
-    # day6 = b.li(text:'6')
-    # day6.click
-    # selday.text.must_equal '6'
   end
 
-  # it 'has working HTML elements' do
-  #   b.text_field(id: 'entry_0').set 'Your Name'
-  #   b.text_field(id: 'entry_0').value.must_equal 'Your Name'
-  #   b.select_list(id: 'entry_1').select 'Ruby'
-  #   b.select_list(id: 'entry_1').value.must_equal 'Ruby'
-  # end
+  it 'quick search' do
+    selday = b.span(id: 'selStay')
+    selday.click
+    day6 = b.li(text:'6')
+    day6.click
+    selday.text.must_equal '6'
+
+    startDate = b.input(id: 'StartDate')
+    startDate.click
+
+    nextMonthBtn = b.a(class: 'ui-datepicker-next ui-corner-all')
+    nextMonthBtn.click
+
+    firstDate = b.a(class: 'ui-state-default')
+    firstDate.click
+
+    endDate = b.input(id: 'EndDate')
+    endDate.click
+
+    nextMonthBtn = b.a(class: 'ui-datepicker-next ui-corner-all')
+    nextMonthBtn.click
+    nextMonthBtn = b.a(class: 'ui-datepicker-next ui-corner-all')
+    nextMonthBtn.click
+
+    secondDate = b.a(class: 'ui-state-default')
+    secondDate.click
+
+    searchBtn = b.input(id: 'btnSearch')
+    searchBtn.click
+
+    resultCount = b.b(id: 'resultCount');
+
+    if (resultCount.text.to_i > 1)
+    	puts resultCount.text
+    else
+    	puts 'Wrong!'
+    end
+
+  end
 end
 
 # describe 'submission works' do
